3b8f9cda4a6e59885dd00332ccedd14f7b52f7c8
developerAbhishek1905 <developer.abhishek1905@gmail.com>
2026-07-08 18:01:21 +0530

    .env config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,13 +9,22 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 // import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// import { initializeApp, cert } from "firebase-admin/app";
+import fs from "fs";
 
-serviceAccount.private_key =
-  serviceAccount.private_key.replace(/\\n/g, "\n");
+let serviceAccount;
 
-const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+} else {
+  serviceAccount = JSON.parse(
+    fs.readFileSync("./src/config/serviceAccountKey.json", "utf8")
+  );
+}
+
+const app = initializeApp({
+  credential: cert(serviceAccount),
 });
 
 const messaging = getMessaging(app);
